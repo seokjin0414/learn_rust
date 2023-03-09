c3b1bc72dc11fc75cf0ed5f3de787a8ad3a8566c
seokjin0414 <sars21@hanmail.net>
2023-03-09 13:42:49 +0900

    p_2 --DONE

--- src/p_2.rs
+++ src/p_2.rs
@@ -1,13 +1,37 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("번호 추측하기!");
-    println!("예측 번호를 입력하세요.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=101);
+    println!("비밀번호 : {secret_number}");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("예측 번호를 입력하세요.");
 
-    println!("your guessed : {}", guess);
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("your guessed : {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
