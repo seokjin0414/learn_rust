669adbcc53a5927e3dc74576f847f48681abc95d
seokjin0414 <sars21@hanmail.net>
2023-03-18 17:16:32 +0900

    p_5_3 --done

---
+++ src/p_5_3_method.rs
@@ -0,0 +1,60 @@
+fn p_5_3_method() {
+    let rect1 = Rectangle {
+        width: 60,
+        height: 70,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 65,
+        height: 60,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    // 같다
+    // p1.distance(&p2); 더 명료
+    // (&p1).distance(&p2);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(9);
+    dbg!(&sq);
+
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
