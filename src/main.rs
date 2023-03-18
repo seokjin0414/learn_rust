3905d50ee6a3f9e49afb6b13ba93a4be80168ef9
seokjin0414 <sars21@hanmail.net>
2023-03-18 16:38:42 +0900

    p_5_3 --ing

--- src/main.rs
+++ src/main.rs
@@ -6,50 +6,27 @@ use std::path::Component::ParentDir;
 use rand::Rng;
 
 fn main() {
-    p_5_2_structs_ex();
+    p_5_3_method();
 }
 
-fn p_5_2_structs_ex() {
-    let width1 = 30;
-    let height1 = 50;
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
-
-    let rect1 = (30, 40);
-    println!("The area of the rectangle is {} square pixels.", area_2(rect1));
-
-    let rect2 = Rectengle {
-        width: 30,
-        height: 30,
+fn p_5_3_method() {
+    let rect1 = Rectangle {
+        width: 60,
+        height: 70,
     };
-    println!("The area of the rectangle is {} square pixels.", area_3(&rect2));
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 
-    println!("rect1 is {:?}", rect2);
-    println!("rect1 is {:#?}", rect2);
 
-    let scale = 2;
-    let rect3 = Rectengle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!(&rect3);
-}
-
-fn area(width: u32, height: u32) -> u32 {
-    width * height
-}
-
-fn area_2(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
-}
-
-fn area_3(rectangle: &Rectengle) -> u32 {
-    rectangle.width * rectangle.height
 }
 
 #[derive(Debug)]
-struct Rectengle {
+struct Rectangle {
     width: u32,
     height: u32,
+}
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
