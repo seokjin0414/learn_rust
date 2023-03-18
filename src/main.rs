bc0e4487bbd90301881dd82722f114fc2c828459
seokjin0414 <sars21@hanmail.net>
2023-03-18 16:16:56 +0900

    p_5_2 ing --DONE

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,16 @@ fn p_5_2_structs_ex() {
     };
     println!("The area of the rectangle is {} square pixels.", area_3(&rect2));
 
+    println!("rect1 is {:?}", rect2);
+    println!("rect1 is {:#?}", rect2);
+
+    let scale = 2;
+    let rect3 = Rectengle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect3);
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -37,6 +47,7 @@ fn area_3(rectangle: &Rectengle) -> u32 {
     rectangle.width * rectangle.height
 }
 
+#[derive(Debug)]
 struct Rectengle {
     width: u32,
     height: u32,
