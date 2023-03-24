f0b44e18dae8ecac1d57315cbc3e4c9630bbc8a0
seokjin0414 <sars21@hanmail.net>
2023-03-24 17:40:05 +0900

    p_6_1 --done

--- src/p_6_1_enum.rs
+++ src/p_6_1_enum.rs
@@ -13,6 +13,17 @@ fn p_6_1_enum() {
     let home = IpAddr2::V4(127, 0, 0, 1);
     let loopback = IpAddr2::V6(String::from("::1"));
 
+    let m = Message::Write(String::from("HAHAHA"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum = x + y;
 }
 
 enum IpAddrKind {
@@ -28,4 +39,28 @@ enum IpAddr {
 enum IpAddr2 {
     V4(u8, u8, u8, u8),
     V6(String),
+}
+
+struct Ipv4Addr {
+}
+
+struct Ipv6Addr {
+}
+
+enum IpAddr3 {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32 , y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
 }
