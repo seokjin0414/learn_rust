e9629144a00f8ae2b10a83f7ede36026ab4f0276
shin <sars21@creativelab-inc.com>
2023-03-29 23:59:30 +0900

    main

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,14 @@ fn main() {
 
 fn p_6_2_match() {
 
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
