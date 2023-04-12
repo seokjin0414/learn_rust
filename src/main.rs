f7647f15035e16f2516b04649e18f860ec262e81
seokjin0414 <sars21@hanmail.net>
2023-04-12 18:42:14 +0900

    coin

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ use aggregator::{Summary, Tweet};
 
 fn main() {
 
-
-
 }
+
+enum Coin {
+    찬원,
+    오천원,
+    만원,
+    오만원,
+}
+
+fn value_in_number(coin: Coin) -> u8 {
+    match coin {
+        Coin::찬원 => 1000,
+        Coin::오천원 => 5000,
+        Coin::만원 => 10000,
+        Coin::오만원 => 50000,
+    }
+}
+
