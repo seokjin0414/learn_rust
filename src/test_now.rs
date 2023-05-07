3e160d6fb424bd60447e7c0dcb24de4d62efa287
shin <sars21@creativelab-inc.com>
2023-05-07 22:55:14 +0900

    type 2

--- src/test_now.rs
+++ src/test_now.rs
@@ -8,3 +8,11 @@ pub trait Write {
     fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
     fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
 }
+
+pub trait Write_2 {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
