ede3bdfe7efe69456846a665992b96de7b65102b
shin <sars21@creativelab-inc.com>
2023-05-07 22:55:36 +0900

    type 3

--- src/test_now.rs
+++ src/test_now.rs
@@ -16,3 +16,12 @@ pub trait Write_2 {
     fn write_all(&mut self, buf: &[u8]) -> Result<()>;
     fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
 }
+
+impl<T> Option<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            Some(val) => val,
+            None => panic!("called `Option::unwrap()` on a `None` value"),
+        }
+    }
+}
